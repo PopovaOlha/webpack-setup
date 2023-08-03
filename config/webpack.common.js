72994f140a93de9efb5b1d824aa253051597d13e
PopovaOlha <fc.odess.10@gmail.com>
2023-08-03 22:13:14 +0300

    feat: scripts for lint

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -6,60 +6,58 @@ const WebpackBar = require('webpackbar');
 const paths = require('./paths');
 
 module.exports = {
-  entry: [paths.src + '/index'],
-  output: {
-    path: paths.dist,
-    filename: '[name].bundle.js',
-    publicPath: '/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: ['babel-loader'],
-      },
-      { test: /\.ts$/i,
-       use: 'ts-loader' 
-      },
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-        type: 'asset/inline',
-      },
-      {
-        test: /\.hbs$/,
-        use: 'handlebars-loader',
-      },
+    entry: [paths.src + '/index'],
+    output: {
+        path: paths.dist,
+        filename: '[name].bundle.js',
+        publicPath: '/',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: ['babel-loader'],
+            },
+            { test: /\.ts$/i, use: 'ts-loader' },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
+            {
+                test: /\.hbs$/,
+                use: 'handlebars-loader',
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new WebpackBar(),
+        new EslingPlugin({
+            extensions: 'ts',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: paths.public,
+                    to: 'assets',
+                    globOptions: {
+                        ignore: ['*.DS_Store'],
+                    },
+                    noErrorOnMissing: true,
+                },
+            ],
+        }),
+        new HtmlWebpackPlugin({
+            template: paths.src + '/index.html',
+            filename: 'index.html',
+        }),
     ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new WebpackBar(),
-    new EslingPlugin({
-       extensions: 'ts'
-       }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: paths.public,
-          to: 'assets',
-          globOptions: {
-            ignore: ['*.DS_Store'],
-          },
-          noErrorOnMissing: true,
-        },
-      ],
-    }),
-    new HtmlWebpackPlugin({
-      template: paths.src + '/index.html',
-      filename: 'index.html',
-    }),
-  ],
-  resolve: {
-    modules: [paths.src, 'node_modules'],
-    extensions: ['.ts', '.js'],
-  },
+    resolve: {
+        modules: [paths.src, 'node_modules'],
+        extensions: ['.ts', '.js'],
+    },
 };
--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -3,56 +3,56 @@ const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
 
 module.exports = merge(commonConfig, {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  // devServer: {
-  //   historyApiFallback: true,
-  //   contentBase: paths.dist,
-  //   compress: true,
-  //   open: true,
-  //   noInfo: true,
-  //   quiet: true,
-  //   clientLogLevel: 'warning',
-  //   stats: 'errors-only',
-  //   hot: true,
-  //   port: 4040,
-  // },
-  devServer: {
-    historyApiFallback: true,
-    open: true,
-    compress: true,
-    hot: true,
-    port: 8080,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(scss|css)$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-              importLoaders: 1,
-              modules: true,
+    mode: 'development',
+    devtool: 'inline-source-map',
+    // devServer: {
+    //   historyApiFallback: true,
+    //   contentBase: paths.dist,
+    //   compress: true,
+    //   open: true,
+    //   noInfo: true,
+    //   quiet: true,
+    //   clientLogLevel: 'warning',
+    //   stats: 'errors-only',
+    //   hot: true,
+    //   port: 4040,
+    // },
+    devServer: {
+        historyApiFallback: true,
+        open: true,
+        compress: true,
+        hot: true,
+        port: 8080,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                            importLoaders: 1,
+                            modules: true,
+                        },
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
             },
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
         ],
-      },
-    ],
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()],
 });
